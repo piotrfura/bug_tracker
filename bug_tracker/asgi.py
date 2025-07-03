89f9fe49e4df3fc11ac655474928313b63d8eb47
Piotr Fura <pf.github@gmail.com>
2025-07-04 01:22:40 +0200

    fix project configs for a static files handling

--- bug_tracker/asgi.py
+++ bug_tracker/asgi.py
@@ -9,8 +9,14 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 
 import os
 
+from django.conf import settings
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 from django.core.asgi import get_asgi_application
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.production')
 
-application = get_asgi_application()
+if settings.DEBUG:
+    application = StaticFilesHandler(get_asgi_application())
+else:
+    application = get_asgi_application()
