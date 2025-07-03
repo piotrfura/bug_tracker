fed5eda3c09e3f2d1bd27d4cbde41cbaf93d2841
Piotr Fura <pf.github@gmail.com>
2025-07-03 15:42:47 +0200

    add test for bug report model

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from main.models import BugReport
 
-# Create your tests here.
+class BugReportModelTest(TestCase):
+    def setUp(self):
+        BugReport.objects.create(title="Test Bug", description="This is a test bug report.")
+
+    def test_bug_report_creation(self):
+        """Test that a BugReport can be created."""
+        bug_report = BugReport.objects.get(title="Test Bug")
+        self.assertEqual(bug_report.description, "This is a test bug report.")
+
+    def test_bug_report_str(self):
+        """Test the string representation of the BugReport model."""
+        bug_report = BugReport.objects.get(title="Test Bug")
+        self.assertEqual(str(bug_report), "Test Bug")
