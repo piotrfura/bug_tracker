0478182e0ebf5f7545158c5b60831992be015beb
Piotr Fura <pf.github@gmail.com>
2025-07-03 15:42:26 +0200

    create bug report model and migrations

--- main/models.py
+++ main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Timestamped(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+# BugReport model with: title, description, created_at
+class BugReport(Timestamped):
+    title = models.CharField(max_length=255, verbose_name="Title")
+    description = models.TextField(verbose_name="Description")
+
+    class Meta:
+        verbose_name = "Bug Report"
+        verbose_name_plural = "Bug Reports"
+
+    def __str__(self):
+        return self.title
