3360a256987172c6506393613bc11ed7cbe7caea
Piotr Fura <pf.github@gmail.com>
2025-07-04 01:30:35 +0200

    add routings for the index and bug report views

---
+++ main/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from main.views import create_bug_report
+from main.views import index
+from main.views import list_bug_reports
+
+
+app_name = "main"
+
+urlpatterns = [
+    path('', index, name='index'),
+    path("bugs/", list_bug_reports, name="list_bug_reports"),
+    path("bugs/create/", create_bug_report, name="create_bug_report"),
+]
