2b21a630f6f39508025d06f8d40db91c4b41014f
Piotr Fura <pf.github@gmail.com>
2025-07-29 22:00:19 +0200

    fix: ensure admin site title is set for Bug Tracker

--- main/urls.py
+++ main/urls.py
@@ -12,3 +12,5 @@ urlpatterns = [
     path("bugs/", list_bug_reports, name="list_bug_reports"),
     path("bugs/create/", create_bug_report, name="create_bug_report"),
 ]
+
+admin.site.site_title = "Bug Tracker Admin Portal"
